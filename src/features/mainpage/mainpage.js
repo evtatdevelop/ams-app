a8497733492aa50c7c4d899d720827d72908c574
Evgenii <evtatdevelop@gmail.com>
2022-11-10 14:09:22 +0700

    flexible layout

--- src/features/mainpage/mainpage.js
+++ src/features/mainpage/mainpage.js
@@ -18,21 +18,21 @@ export const Mainpage = () => {
     <section className={styles.mainpage}>
 
       <header className={styles.header}>
-        <div>
-          <h1 className={styles.name}>{dictionaryData.head_systemname}</h1>
-          
-          {dictionaryData.head_currentuser && userData.ad_user
-            ? <p className={styles.remoteUser}> {`${dictionaryData.head_currentuser} ${userData.shortname} (${userData.ad_user})`} </p>
-            : null
-          }
 
-          <button type='button'
-            className={styles.langSwitcher}
-            onClick={() => {dispatch(setLang( {'app12_id': userData['id'], 'lang': userData['lang'] === 'RU' ? 'EN' : 'RU'} ))}}
-          >{userData['lang'] === 'RU' ? 'En' : 'Ru'}</button>            
-        </div>
+        <h1 className={styles.head_systemname}>{dictionaryData.head_systemname}</h1>
+        
+        {dictionaryData.head_currentuser && userData.ad_user
+          ? <p className={styles.remoteUser}> {`${dictionaryData.head_currentuser} ${userData.shortname} (${userData.ad_user})`} </p>
+          : null
+        }
 
-     
+        <input className={styles.search} type="search" id="search" name="search" placeholder="Search"/>
+
+        <button type='button'
+          className={styles.langSwitcher}
+          onClick={() => {dispatch(setLang( {'app12_id': userData['id'], 'lang': userData['lang'] === 'RU' ? 'EN' : 'RU'} ))}}
+        >{userData['lang'] === 'RU' ? 'En' : 'Ru'}</button>            
+
       </header>
 
       
