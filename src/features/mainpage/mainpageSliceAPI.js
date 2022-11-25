694337e4a37e756dcba3350f143eae2edc4466af
Evgenii <evtatdevelop@gmail.com>
2022-11-25 14:36:52 +0700

    codeClinning

--- src/features/mainpage/mainpageSliceAPI.js
+++ src/features/mainpage/mainpageSliceAPI.js
@@ -7,7 +7,4 @@ const _apiBase = testMode
 ? 'https://request.sibgenco.local/ams_api_tst'
 : 'https://request.sibgenco.local/ams_api';
 
-export const getMainpageData = ( api_key ) => {
-  // console.log('getMainpageData', api_key);
-  return service.getResource(`${_apiBase}/?q=mainpage`, api_key)
-};
+export const getMainpageData = ( api_key ) => service.getResource(`${_apiBase}/?q=mainpage`, api_key);
