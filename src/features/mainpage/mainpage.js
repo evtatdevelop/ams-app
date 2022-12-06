b6c9c3c2fdb3ede86d3d099e5dc822aa6f05b316
Evgenii <evtatdevelop@gmail.com>
2022-12-06 13:27:56 +0700

    add header and footer

--- src/features/mainpage/mainpage.js
+++ src/features/mainpage/mainpage.js
@@ -8,6 +8,18 @@ import LangButton from "./langButton";
 // import SearchSystems from "./search";
 import SearchList from "./searchList";
 import ExpirationScreen from "../expirationScreen";
+import bottomLeftImg from '../../assets/bottomleftbg2.png';
+import bottomRightImg from '../../assets/bottomrightbg1.png';
+
+import stemLogoRU from '../../assets/STEMRU.png';
+import suekLogoRU from '../../assets/scec.svg';
+import sgkLogoRU from '../../assets/sgc.svg';
+import ntkLogoRU from '../../assets/ntc.svg';
+import ehLogo from '../../assets/eh.svg';
+import stemLogoEN from '../../assets/STEMEN.png';
+import suekLogoEN from '../../assets/suek.svg';
+import sgkLogoEN from '../../assets/sgk.png';
+import ntkLogoEN from '../../assets/ntk.png';
 
 export const Mainpage = () => {
 
@@ -25,7 +37,24 @@ export const Mainpage = () => {
 
   return (
     <section className={styles.mainpage}>
-      <header></header>
+      <header>
+        {userData['lang'] === 'RU'
+          ? <>
+              <img src={stemLogoRU} />
+              <img src={suekLogoRU} />
+              <img src={sgkLogoRU} />
+              <img src={ntkLogoRU} />
+              <img src={ehLogo} />
+            </>
+          : <>
+              <img src={stemLogoEN} />
+              <img src={suekLogoEN} />
+              <img src={sgkLogoEN} />
+              <img src={ntkLogoEN} />
+              <img src={ehLogo} />
+            </>
+        }
+      </header>
       
       <main>
       <LangButton/>
@@ -46,7 +75,10 @@ export const Mainpage = () => {
       
       </main>
       
-      <footer></footer>
+      <footer>
+        <img src={bottomLeftImg} />
+        <img src={bottomRightImg} />
+      </footer>
  
 
       { expired ? <ExpirationScreen/> : null }
