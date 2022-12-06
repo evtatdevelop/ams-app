1c79d1a6379a8f01af5c3f9233d56048f70540ef
Evgenii <evtatdevelop@gmail.com>
2022-12-06 11:10:43 +0700

    common template

--- src/features/mainpage/mainpage.js
+++ src/features/mainpage/mainpage.js
@@ -4,9 +4,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { mainpage, dictionary, getMainpage, search } from "./mainpageSlice";
 import { user } from '../user/userSlice';
 import Section from "./section";
-import RowSection from "./rowSection";
 import LangButton from "./langButton";
-import SearchSystems from "./search";
+// import SearchSystems from "./search";
 import SearchList from "./searchList";
 import ExpirationScreen from "../expirationScreen";
 
@@ -26,32 +25,29 @@ export const Mainpage = () => {
 
   return (
     <section className={styles.mainpage}>
-      <header className={styles.header}>
+      <header></header>
+      
+      <main>
+      <LangButton/>
         <h1 className={styles.head_systemname}>{dictionaryData.head_systemname}</h1>
         {dictionaryData.head_currentuser && userData.ad_user
-          ? <div className={styles.remoteUser}> <p>{`${dictionaryData.head_currentuser}`}<br/>{`${userData.shortname} (${userData.ad_user})`}</p></div>
+          ? <div className={styles.remoteUser}> {`${dictionaryData.head_currentuser}`} {`${userData.shortname} (${userData.ad_user})`}</div>
           : null
         }
-        <SearchSystems/>
-        <LangButton/>
-      </header>
 
-      {searchString === "" 
-        ? <ul className={styles.syetems}>
-            {/* <RowSection 
-              key='qweqwq' 
-              sections={pageData.filter(section => section.prefix === 'LK' || section.prefix === 'CORPORATE')
-            }/>
-            { pageData
-              .filter(section => section.prefix !== 'LK' && section.prefix !== 'CORPORATE')
-              .map(section => <Section key={section.id} section={section}/>)
-            } */}
-            { pageData.map(section => <Section key={section.id} section={section}/>) }
-          
-          </ul>
+        {/* <SearchSystems/> */}
 
-        : <SearchList/>  
-      }  
+        {searchString === "" 
+          ? <ul className={styles.syetems}>
+              { pageData.map(section => <Section key={section.id} section={section}/>) }
+            </ul>
+          : <SearchList/>  
+        }       
+      
+      </main>
+      
+      <footer></footer>
+ 
 
       { expired ? <ExpirationScreen/> : null }
       
