5cca3f2732223c29ab5f63e09669bfb94465baaa
Evgenii <evtatdevelop@gmail.com>
2023-04-21 09:50:45 +0700

    save sectiont show condition for mainpage

--- src/features/mainpage/section/section.js
+++ src/features/mainpage/section/section.js
@@ -6,27 +6,39 @@ import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
 
 export const Section = props => {
   const { section } = props;
-  const [show, onShow] = useState(section.prefix !== 'LK' && section.prefix !== 'TOP_ORDERS' ? true : false);
+ 
+  const sectionHide = localStorage[section.prefix] 
+    ? localStorage[section.prefix] === 'true' ? true : false
+    : section.prefix === 'LK' || section.prefix === 'TOP_ORDERS' ? false : true
+
+  const [hide, onHide] = useState(sectionHide);
+  localStorage[section.prefix] = sectionHide;
+
+  const showHide = () => {
+    if ( hide ) document.getElementById(section.id).scrollIntoView('top');
+    onHide(!hide)
+    localStorage[section.prefix] = !hide;
+  }
 
   return (
     <li className={styles.section} id={section.id}>
 
       <header>
         <label htmlFor={section.prefix}>
-          {/* <h2 className={styles.name}>{section.name} ({Object.keys(section.systems).length})</h2> */}
           <h2 className={styles.name_sf}>{section.name} ({Object.keys(section.systems).length})</h2>
-          <FontAwesomeIcon icon={ faCaretDown } className={show ? styles.iconButtonClose : styles.iconButton} />
+          <FontAwesomeIcon icon={ faCaretDown } className={hide ? styles.iconButtonClose : styles.iconButton} />
         </label>
         
       </header>
       
       <input type="checkbox" 
         id={section.prefix}
-        checked={show}
-        onChange={()=>{
-          if ( show ) document.getElementById(section.id).scrollIntoView('top');
-          return onShow(!show)
-        }}
+        checked={hide}
+        onChange={()=>showHide()}
+        // onChange={()=>{
+        //   if ( hide ) document.getElementById(section.id).scrollIntoView('top');
+        //   return onHide(!hide)
+        // }}
       /> 
       <ul className={styles.sectionList}>
         {section.systems.map((system, index) => {
