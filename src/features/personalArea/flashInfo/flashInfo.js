017dbcc933abe8dae648167730f7fb4f35cfb8e0
Evgenii <evtatdevelop@gmail.com>
2023-07-07 17:02:46 +0700

    flashInfo for WP and FSR

--- src/features/personalArea/flashInfo/flashInfo.js
+++ src/features/personalArea/flashInfo/flashInfo.js
@@ -13,8 +13,6 @@ export const FlasInfo = () => {
   const [h, seth] = useState(0)
   const [w, setw] = useState(0)
 
-  // console.log(w, h);
-
   useEffect(()=> {
     if ( showOI ) {
       seth(ref.current ? ref.current.clientHeight : 0)
@@ -22,14 +20,65 @@ export const FlasInfo = () => {
     }
   },[showOI])
 
-  const X = document.documentElement.clientWidth < leftOI + w ? `${leftOI}px` : `${leftOI}px`;
-  const Y = document.documentElement.clientHeight < bottomOI + h ? `${topOI-h}px` : `${bottomOI}px`;
+  const X = document.documentElement.clientWidth < leftOI + w ? `${document.documentElement.clientWidth - w}px` : `${leftOI}px`;
+  const Y = document.documentElement.clientHeight < bottomOI + h + 3 ? `${topOI-h}px` : `${bottomOI}px`;
+  const W = document.documentElement.clientWidth < leftOI + w && document.documentElement.clientWidth - w < 0 ? `100vw` : `auto`;
+
   const styleOrderInfo = showOI ? `${styles.orderInfo} ${styles.show}` : `${styles.orderInfo}`
 
   return (
     showOI
-      ? <div className={styleOrderInfo} style={{left: X, top: Y}} ref={ref}>
-        {dataOI.status}
+      ? <div className={styleOrderInfo} style={{left: X, top: Y, width: W }} ref={ref}>
+        {/* { dataOI.order_type === 'WORKPLACE' */}
+        { true
+          ? <ul className={styles.orderInfoList}>
+              {dataOI.status ? <li><span className={styles.label}>Статус запроса:</span> <span className={styles[dataOI.status_color]}>{dataOI.status}</span></li> : null}
+              {dataOI.date_open ? <li><span className={styles.label}>Дата подачи:</span> {dataOI.date_open}</li> : null}
+              { dataOI.api_order_user
+                ? <>
+                    { dataOI.api_order_user.placement ? <li><span className={styles.label}>Тип трудоустройства:</span> {dataOI.api_order_user.placement}</li> : null}
+                    { dataOI.api_order_user.name 
+                      ? <li>
+                          <span className={styles.label}>Cотрудник:</span> {dataOI.api_order_user.name}
+                          <p>
+                            { dataOI.api_order_user.branch || dataOI.api_order_user.position || dataOI.api_order_user.division
+                              ? `${dataOI.api_order_user.branch ? dataOI.api_order_user.branch : ''} ${dataOI.api_order_user.position ? dataOI.api_order_user.position : ''} ${dataOI.api_order_user.division ? dataOI.api_order_user.division : ''}`
+                              : null
+                            } 
+                          </p>
+                          <p> 
+                            {dataOI.api_order_user.outer_company_name ? dataOI.api_order_user.outer_company_name : null} 
+                          </p>
+                        </li> 
+                      : null
+                    }
+
+                    
+                    
+
+                  </>
+                : null
+              }
+
+              { dataOI.order_type === 'FSR'
+                ? <>{ dataOI.api_privs
+                    ? <li>
+                        {dataOI.api_privs.type_act === 'NEW' ? 'Создание' : 'Изменение'} {dataOI.api_privs.type_code === 'FILE' ? 'файлового ресурса' : 'серверного ресурса'}
+                        { dataOI.api_privs.path 
+                          ? <p>{dataOI.api_privs.path}</p>
+                          : null
+                        }
+                      </li>
+                    : null
+                  }</>
+                : null}
+              
+            </ul>         
+
+          : null
+
+        }
+
         
         </div>
       : null
